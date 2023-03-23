449386ddb8f3f79f09624bb5ed520d949845180c
Kim Minh Thang <thangved02@gmail.com>
2023-03-23 15:15:30 +0700

    Fix websocket

--- lib/configs/http_config.dart
+++ lib/configs/http_config.dart
@@ -1,5 +1,5 @@
-// const String baseUrl = "https://api.messeenger.thangved.com/api";
-// const String socketUrl = "https://websocket.messeenger.thangved.com";
+const String baseUrl = "https://api.messeenger.thangved.com/api";
+const String socketUrl = "https://websocket.messeenger.thangved.com";
 
-const baseUrl = "http://172.17.0.1:5000/api";
-const socketUrl = "http://172.17.0.1:5001";
+// const baseUrl = "http://172.17.0.1:5000/api";
+// const socketUrl = "http://172.17.0.1:5001";
